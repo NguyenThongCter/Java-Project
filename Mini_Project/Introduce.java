3186bea37fb72ee8f3a6ffa8e197df324a767a73
Nguyễn Thông <thongnguyendev75@gmail.com>
2026-04-28 09:30:58 +0700

    Add files via upload

---
+++ Mini_Project/Introduce.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Introduce {
+          public static void main(String[] args) {
+                    int Age = 20;
+                    float kg = 50.5f;
+                    System.out.println("Enter your name: ");
+                    Scanner sc = new Scanner(System.in);
+                    String hoVaten = sc.nextLine();
+
+                    System.out.println("i'm todicodelai!!");
+                    System.out.println("\ni'm "+ Age +" years old");
+                    System.out.println("I'm weight "+kg+"kg");
+          }
+}
