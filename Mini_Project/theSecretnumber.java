bed8ed18387ee580caf4dec60e28b343dc661644
Nguyễn Thông <thongnguyendev75@gmail.com>
2026-05-12 21:07:42 +0700

    Add files via upload

---
+++ Mini_Project/theSecretnumber.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class theSecretnumber {
+          public static void main(String[] args) {
+                    Scanner sc = new Scanner(System.in);
+
+                    // Random number (1 - 100)
+                    int secret = (int)(Math.random() * 100)+ 1;
+                    int guess;
+                    int count = 0;
+
+                    System.out.println("=== THE SECRET NUMBER GAME ===");
+                    System.out.println("I have picked a number between 1 and 100. Can you guess it?");
+                    do{
+                              System.out.println("Enter each guess: ");
+                              guess = sc.nextInt();
+                              count++;
+                              // If wrong guess: suggest max/min
+                              if(guess > secret){
+                                        System.out.println("Too high! Try a smaller number!!");
+                              }else if(guess < secret){
+                                        System.out.println("Too low! Try a larger number!!");
+                              }else{
+                                        System.out.println("------------------------------------");
+                                        System.out.println("CONGRATULATIONS! You found it: "+secret);
+                                        System.out.println("Total attempts: "+count);
+
+                                        // Bonus rank after guess
+                                        if(count <= 5){
+                                                  System.out.println("Rank: King guess!!");
+                                        }else if (count <= 10){
+                                                  System.out.println("Rank: Guess Good");
+                                        }else{
+                                                  System.out.println("Need some Lucky!!");
+                                        }
+                              }
+                    }while(guess != secret);
+                    sc.close();
+          }
+}
