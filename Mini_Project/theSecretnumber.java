edb422f50c523f5f431637ec0cd36e8448dcc1a9
Nguyễn Thông <thongnguyendev75@gmail.com>
2026-05-15 10:59:34 +0700

    Implement guess history tracking in the game
    Added functionality to track and display guess history.

--- Mini_Project/theSecretnumber.java
+++ Mini_Project/theSecretnumber.java
@@ -3,28 +3,34 @@ import java.util.Scanner;
 public class theSecretnumber {
           public static void main(String[] args) {
                     Scanner sc = new Scanner(System.in);
-
+                    // Update them ve mang
+                    int [] history = new int[100];
                     // Random number (1 - 100)
-                    int secret = (int)(Math.random() * 100)+ 1;
-                    int guess;
+                    int secret = (int)(Math.random() * 100) + 1;
+                    int currguess;
                     int count = 0;
 
                     System.out.println("=== THE SECRET NUMBER GAME ===");
                     System.out.println("I have picked a number between 1 and 100. Can you guess it?");
                     do{
                               System.out.println("Enter each guess: ");
-                              guess = sc.nextInt();
+                              currguess = sc.nextInt();
+                              history[count] = currguess;
                               count++;
                               // If wrong guess: suggest max/min
-                              if(guess > secret){
+                              if(currguess > secret){
                                         System.out.println("Too high! Try a smaller number!!");
-                              }else if(guess < secret){
+                              }else if(currguess < secret){
                                         System.out.println("Too low! Try a larger number!!");
                               }else{
                                         System.out.println("------------------------------------");
                                         System.out.println("CONGRATULATIONS! You found it: "+secret);
                                         System.out.println("Total attempts: "+count);
-
+                                        // History guessing
+                                        System.out.println("Your Guessing History: ");
+                                        for(int i = 0; i < count; i++){
+                                                  System.out.println(history[i] + "");
+                                        }
                                         // Bonus rank after guess
                                         if(count <= 5){
                                                   System.out.println("Rank: King guess!!");
@@ -34,7 +40,7 @@ public class theSecretnumber {
                                                   System.out.println("Need some Lucky!!");
                                         }
                               }
-                    }while(guess != secret);
+                    }while(currguess != secret);
                     sc.close();
           }
 }
